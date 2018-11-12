5bbc1ee494e3b9fedca0eb19d6601555fbb1acb8
Christian Jaede Samboa <cjsmaboa@gmail.com>
2018-11-12 16:15:41 +0800

    Added Function

--- server/routes/indexRouter.js
+++ server/routes/indexRouter.js
@@ -5,8 +5,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('Hello World!');
+router.get('/index', (req, res) => {
+    res.render('register.pug');
 })
 
 module.exports = router;

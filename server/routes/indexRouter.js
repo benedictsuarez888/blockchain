e7465fd3433900be97f415c1de68e8342672a766
Joshua Soriano <jessoriano24@gmail.com>
2018-11-12 17:04:25 +0800

    fixed conflict

--- server/routes/indexRouter.js
+++ server/routes/indexRouter.js
@@ -6,7 +6,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("index.pug")
+    res.render("login.pug")
+})
+
+router.get('/index', (req, res) => {
+    res.render("register.pug")
 })
 
 module.exports = router;
